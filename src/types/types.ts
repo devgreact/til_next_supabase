8d52ab2a374e69d1b607d5b1593d04244bd0cf81
devgreact <dev.greact@gmail.com>
2025-11-13 10:42:29 +0900

    [docs] 포스트 이미지 등록, 내용 출력

--- src/types/types.ts
+++ src/types/types.ts
@@ -4,6 +4,7 @@ export type PostEntity = Database['public']['Tables']['posts']['Row'];
 export type InsertPostEntity = Database['public']['Tables']['posts']['Insert'];
 export type UpdatePostEntity = Database['public']['Tables']['posts']['Update'];
 export type PostTableEntity = Database['public']['Tables']['posts'];
+
 // 프로필 관련
 export type ProfileEntity = Database['public']['Tables']['profiles']['Row'];
 export type InsertProfileEntity =
@@ -11,6 +12,9 @@ export type InsertProfileEntity =
 export type UpdateProfileEntity =
   Database['public']['Tables']['profiles']['Update'];
 export type ProfileTableEntity = Database['public']['Tables']['profiles'];
+
+// 포스트와 프로필 타입 조합
+export type Post = PostEntity & { author: ProfileEntity };
 
 export type UseMutationCallback = {
   onError?: (error: Error) => void;
